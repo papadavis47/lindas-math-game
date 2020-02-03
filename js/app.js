687cdbe86214c3e8670f2b2b36f261c3f1961cc6
John William Davis <jwdavisdeveloper@gmail.com>
2020-02-03 12:10:54 -0800

    add some js, build html game transitions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
+let score = 0;
+
+let addition = [
+  {
+    problem: "12 + 2 =",
+    answer: 14
+  },
+
+  {
+    problem: "8 + 20 =",
+    answer: 28
+  },
+
+  {
+    problem: "10 + 22 =",
+    answer: 32
+  },
+
+  {
+    problem: "19 + 7 =",
+    answer: 26
+  },
+
+  {
+    problem: "16 + 30 =",
+    answer: 46
+  },
+
+  {
+      problem: "15 + 23 =",
+      answer: 38
+  },
+
+  {
+      problem: "17 + 4 =",
+      answer: 21
+  },
+
+  {
+      problem: "19 + 23 =",
+      answer: 42
+  },
+];
+
 console.log("I love you, Linda - Hija Bonita!");
