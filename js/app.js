608b4110396a332ac67f41408b799be210dabc29
John William Davis <jwdavisdeveloper@gmail.com>
2020-02-03 17:03:48 -0800

    test js

--- js/app.js
+++ js/app.js
@@ -27,19 +27,33 @@ let addition = [
   },
 
   {
-      problem: "15 + 23 =",
-      answer: 38
+    problem: "15 + 23 =",
+    answer: 38
   },
 
   {
-      problem: "17 + 4 =",
-      answer: 21
+    problem: "17 + 4 =",
+    answer: 21
   },
 
   {
-      problem: "19 + 23 =",
-      answer: 42
-  },
+    problem: "19 + 23 =",
+    answer: 42
+  }
 ];
+
+// I found out I need to declare my variables within DOMCONTENT - function
+
+document.addEventListener("DOMContentLoaded", function(e) {
+  let hide1 = document.getElementById("hide-1");
+  let hide2 = document.getElementById("hide-2");
+  let hide3 = document.getElementById("hide-3");
+  let splashScreen = document.getElementsByClassName("splash-screen");
+
+  hide1.addEventListener("click", function(e) {
+    hide1.style.display = "none";
+    hide2.style.display = "block";
+  });
+});
 
 console.log("I love you, Linda - Hija Bonita!");
