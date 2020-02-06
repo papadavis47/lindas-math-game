d631dfdf97f3687b9303533aa3316a4b62b78fc9
John William Davis <jwdavisdeveloper@gmail.com>
2020-02-06 13:20:19 -0800

    add Easter Egg

--- js/about.js
+++ js/about.js
@@ -1,1 +1,14 @@
-console.log("Page for fun stuff");
+
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("Page for fun stuff");
+
+    document.addEventListener("keydown", function(event) {
+        if (event.keyCode === 55) {
+            document.getElementById("thanks").play();
+        }
+    })
+
+
+
+});
